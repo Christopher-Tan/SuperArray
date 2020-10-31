ccb1c426fa578e360a50704876435431836e7c88
Christopher Tan <ctan20@stuy.edu>
2020-10-31 17:36:26 -0400

    Added and Tested size method

--- SuperArray.java
+++ SuperArray.java
@@ -6,4 +6,7 @@ public class SuperArray {
         data = new String[10];
         size = 0;
     }
+    public int size() {
+        return size;
+    }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester {
     public static void main(String[] args) {
         SuperArray test = new SuperArray();
+        System.out.println(test.size());
     }
 }
