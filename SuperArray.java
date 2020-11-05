c84bb09eac1f0a62834d64a4460898cb81021fd4
Christopher Tan <ctan20@stuy.edu>
2020-11-05 14:07:03 -0500

    Added and tested findoverlap method

--- SuperArray.java
+++ SuperArray.java
@@ -104,4 +104,14 @@ public class SuperArray {
             }
         }
     }
+    public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+        SuperArray result = new SuperArray();
+        for (int i = 0; i < a.size; i++) {
+            if (b.contains(a.get(i))) {
+                result.add(a.get(i));
+            }
+        }
+        removeDuplicates(result);
+        return result;
+    }
 }
